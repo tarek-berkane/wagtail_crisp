5ab26dc29f57ecfeb252e00fde1e1c793830d30b
tarek-berkane <riko.jsk.320@gmail.com>
2022-07-17 13:17:54 +0100

    add pagination

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.paginator import Paginator
 
 from wagtail.admin.panels import StreamFieldPanel, FieldPanel
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField, RichTextField
 from wagtail.core.blocks import RichTextBlock
 from wagtail.contrib.table_block.blocks import TableBlock
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 
 from apps.post.blocks import HeaderBlock, ImageBlock, QuoteBlock, CodeBlock
 
@@ -19,13 +21,31 @@ RICH_TEXT_FEATURES = [
 ]
 
 
-class CategoryIndex(Page):
+class CategoryIndex(RoutablePageMixin, Page):
     max_count = 1
     parent_page_types = ["home.Home"]
     subpage_types = ["post.PostCategory"]
 
+    @route(r"^$")
+    def current_events(self, request):
+        post = PostCategory.objects.all().live()
+        paginator = Paginator(post, 8)
 
-class PostCategory(Page):
+        page_number = request.GET.get("p")
+        if page_number:
+            result = paginator.get_page(page_number)
+        else:
+            result = paginator.get_page(1)
+
+        return self.render(
+            request,
+            context_overrides={
+                "result": result,
+            },
+        )
+
+
+class PostCategory(RoutablePageMixin, Page):
     parent_page_types = ["post.CategoryIndex"]
     subpage_types = ["post.Post"]
 
@@ -34,6 +54,24 @@ class PostCategory(Page):
     content_panels = Page.content_panels + [
         FieldPanel("description"),
     ]
+
+    @route(r"^$")
+    def current_events(self, request):
+        post = self.get_children().live()
+        paginator = Paginator(post, 8)
+
+        page_number = request.GET.get("p")
+        if page_number:
+            result = paginator.get_page(page_number)
+        else:
+            result = paginator.get_page(1)
+
+        return self.render(
+            request,
+            context_overrides={
+                "result": result,
+            },
+        )
 
 
 class Post(Page):

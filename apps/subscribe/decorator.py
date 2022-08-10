4aecfdfa930bdff937036dbd4ce6b46374f4b621
tarek-berkane <riko.jsk.320@gmail.com>
2022-08-11 00:26:14 +0100

    add subscribe session store

--- apps/subscribe/decorator.py
+++ apps/subscribe/decorator.py
@@ -1,6 +1,8 @@
 from functools import wraps
 from django.http import HttpRequest, HttpResponseBadRequest
 
+from apps.subscribe.constants import ALREADY_SUBSCRIBE, SUBSCRIBE_COUNT
+
 
 def subscribe_limit():
     """
@@ -10,13 +12,15 @@ def subscribe_limit():
     def decorator(func):
         @wraps(func)
         def inner(request: HttpRequest, *args, **kwargs):
-            if subscribe_count := request.session.get("subscribe_count"):
+            if subscribe_count := request.session.get(SUBSCRIBE_COUNT):
                 if subscribe_count > 3:
+                    request.session[ALREADY_SUBSCRIBE] = True
                     return HttpResponseBadRequest()
-                count = request.session["subscribe_count"] + 1
+                count = request.session[SUBSCRIBE_COUNT] + 1
             else:
                 count = 1
-            request.session["subscribe_count"] = count
+            request.session[SUBSCRIBE_COUNT] = count
+
             return func(request, *args, **kwargs)
 
         return inner
--- apps/subscribe/views.py
+++ apps/subscribe/views.py
@@ -2,6 +2,7 @@ from functools import wraps
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponseBadRequest, HttpResponse
 from django.views.decorators.http import require_http_methods
+from apps.subscribe.constants import ALREADY_SUBSCRIBE
 from apps.subscribe.decorator import subscribe_limit
 
 from apps.subscribe.forms import SubscribeForm
@@ -13,4 +14,4 @@ def subscribe(request: HttpRequest):
     form = SubscribeForm(request.POST)
     if form.is_valid():
         form.save()
-        return HttpResponse("Done!")
+    return HttpResponse("Done!")
